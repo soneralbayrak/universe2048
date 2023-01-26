5fa56a08b69c1d1ae51938b00b66d5999740980f
Soner Albayrak <snralbayrak@gmail.com>
2023-01-26 15:00:49 +0100

    commit

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "BBCM <3" : "BBCM :(";
+  var message = won ? "You win!" : "You lose!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
